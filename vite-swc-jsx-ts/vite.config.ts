dac8ab92ed6f432b7980d0d23d6f9ed0238d3f18
Botond Szabo <112097901+botond-szabo@users.noreply.github.com>
2024-12-08 19:36:15 -0700

    Update vite.config.ts

--- vite-swc-jsx-ts/vite.config.ts
+++ vite-swc-jsx-ts/vite.config.ts
@@ -1,9 +1,15 @@
 import path from "@necto/path";
-import { defineConfig } from 'vite';
-import react from "@necto/vite/plugins/react"
+import { defineConfig } from "vite";
+import pug from "@necto/vite/plugins/pug";
+import glsl from "@necto/vite/plugins/glsl";
+import wasm from "@necto/vite/plugins/wasm";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    pug(),
+    wasm(),
+    glsl()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
